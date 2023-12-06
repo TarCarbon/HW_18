ddb13b7cc2069eff0316a256c0383ebc0b657f0b
TarCarbon <metalcarbon58@gmail.com>
2023-12-06 17:05:33 +0100

    add WebConfig in config package

---
+++ src/main/java/com/example/entity/Note.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String title;
+    @Column
+    private String context;
+}
